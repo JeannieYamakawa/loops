667b974eced0dc657d429d9c96f22373c6545553
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-08-12 15:00:41 -0500

    fixes syntax to look prettier

--- looping-prac.js
+++ looping-prac.js
@@ -1,10 +1,26 @@
 
 
 
-for (var i = 0; i < 11; i++) {console.log(i * 100);}
-for (var i = 0; i <= 7; i++) {console.log(Math.pow(2, i));}
-for (var i = 0; i < 6; i++) {console.log(i * 2);}
-for (var i = 0; i < 6; i++) {console.log(i * 3);}
-for (var i = 9; i >= 0; i--) {console.log(i);}
-for (var i = 0; i < 5; i++){console.log(i);console.log(i);console.log(i)}
-for (var i = 0; i < 3; i++) {for (var printi = 0; printi<5; printi++){console.log(printi)}}
+for (var i = 0; i < 11; i++) {
+  console.log(i * 100);
+}
+for (var i = 0; i <= 7; i++) {
+  console.log(Math.pow(2, i));
+}
+for (var i = 0; i < 6; i++) {
+  console.log(i * 2);
+}
+for (var i = 0; i < 6; i++) {
+  console.log(i * 3);
+}
+for (var i = 9; i >= 0; i--) {
+  console.log(i);
+}
+for (var i = 0; i < 5; i++){
+  console.log(i);console.log(i);console.log(i)
+}
+for (var i = 0; i < 3; i++) {
+  for (var printi = 0; printi<5; printi++){
+    console.log(printi)
+  }
+}
